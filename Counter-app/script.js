5afd00d228bb697e122691aa22d891f22ddc3eb4
Kittur <aishwarya.k.kittur@gmail.com>
2025-01-19 18:41:44 +0530

    modified Counter-app

--- Counter-app/script.js
+++ Counter-app/script.js
@@ -1,21 +1,21 @@
-
+let currentCounter = document.getElementById("currentCounter").innerText
 
 function decreaseCount(){
-    let currentCounter = document.getElementById("currentCounter").innerText
+    //let currentCounter = document.getElementById("currentCounter").innerText
     currentCounter =  parseInt(currentCounter) - 1
     document.getElementById("currentCounter").innerHTML = currentCounter
     document.getElementById("currentCounter").style.color = "#eb1313"
 }
 
 function increaseCount(){
-    let currentCounter = document.getElementById("currentCounter").innerText
+    //let currentCounter = document.getElementById("currentCounter").innerText
     currentCounter =  parseInt(currentCounter) + 1
     document.getElementById("currentCounter").innerHTML = currentCounter
     document.getElementById("currentCounter").style.color = "#37eb13"
 }
 
 function resetCount(){
-    let currentCounter = document.getElementById("currentCounter").innerText
+    //let currentCounter = document.getElementById("currentCounter").innerText
     currentCounter =  0
     document.getElementById("currentCounter").innerHTML = currentCounter
     document.getElementById("currentCounter").style.color = "#0c0d0c"
